2a210cb9a0646c10820c4a56f3e5e5a256468ee4
nbuechler <natebuechler@gmail.com>
2015-11-17 00:29:20 -0500

    fixed hinting erros

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -22,12 +22,12 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 					// /**
 	        //  * TODO: It may be worth considering creating a service for this to handle all error codes... but later.
 	        //  */
-	        if(response.customCode == 2001){
+	        if(response.customCode === 2001){
 						// Redirect to the index page
 	          $location.path('/');
 						// If successful we assign the response to the global user model
 	          localStorage.setItem('currentSession', 1);
-						console.log(response)
+						console.log(response);
 	        // } else if (response.customCode == 4031) {
 	        //   self.setState({showError: true});
 	        //   self.setState({message: response.errors[0].msg});

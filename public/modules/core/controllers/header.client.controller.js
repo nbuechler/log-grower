b3162d0a5277ad822ae996dbf6b54940fb880fc8
nbuechler <natebuechler@gmail.com>
2015-11-23 22:23:52 -0500

    front end detection for signout

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Menus',
-	function($scope, Menus) {
-		$scope.authentication = 0;
+angular.module('core').controller('HeaderController', ['$scope', '$http', '$window', 'Menus',
+	function($scope, $http, $window, Menus) {
+		$scope.authentication = localStorage.getItem('currentSession');
 		$scope.lastCredentials = localStorage.getItem('credentials');
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
@@ -15,5 +15,17 @@ angular.module('core').controller('HeaderController', ['$scope', 'Menus',
 		$scope.$on('$stateChangeSuccess', function() {
 			$scope.isCollapsed = false;
 		});
+
+		$scope.logout = function() {
+
+			$http.get('/auth/signout').success(function(response) {
+				localStorage.setItem('currentSession', 0);
+				localStorage.setItem('credentials', null);
+				$window.location.reload();
+			}).error(function(response) {
+				$scope.error = response.message;
+			});
+		};
+
 	}
 ]);

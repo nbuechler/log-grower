2110cb683e7393d7c7381a76ae1bb216a25a0c8c
nbuechler <natebuechler@gmail.com>
2015-11-24 23:31:49 -0500

    added activities module

---
+++ public/modules/activities/controllers/activities.client.controller.js
@@ -0,0 +1,100 @@
+'use strict';
+
+// Activities controller
+angular.module('activities').controller('ActivitiesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Activities', 'PublicActivities',
+	function($scope, $stateParams, $location, Authentication, Activities, PublicActivities) {
+		$scope.authentication = Authentication;
+
+		// Create new Activity
+		$scope.create = function() {
+			// Create new Activity object
+			var activity = new Activities ({
+				name: this.name,
+                description: this.description,
+                importance: this.importance,
+								privacy: this.privacy ? this.privacy : 0
+			});
+
+			// Redirect after save
+			activity.$save(function(response) {
+				$location.path('activities/' + response._id);
+
+				// Clear form fields
+				$scope.name = '';
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Remove existing Activity
+		$scope.remove = function(activity) {
+			if ( activity ) {
+				activity.$remove();
+
+				for (var i in $scope.activities) {
+					if ($scope.activities [i] === activity) {
+						$scope.activities.splice(i, 1);
+					}
+				}
+			} else {
+				$scope.activity.$remove(function() {
+					$location.path('activities');
+				});
+			}
+		};
+
+		// Update existing Activity
+		$scope.update = function() {
+
+			$scope.activity.privacy = $scope.activity.privacy ? $scope.activity.privacy : 0;
+
+			var activity = $scope.activity;
+
+			activity.$update(function() {
+				$location.path('activities/' + activity._id);
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+        // Set marker for creating new activity... for first time users
+        $scope.displayFirstTime = true; //not used
+
+		// Find a list of Activities
+		$scope.find = function() {
+			$scope.activities = Activities.query();
+		};
+
+		// Find a list of PublicActivities
+		$scope.findPublic = function() {
+			$scope.activities = PublicActivities.query();
+		};
+
+		// Find existing Activity
+		$scope.findOne = function() {
+			$scope.activity = Activities.get({
+				activityId: $stateParams.activityId
+			});
+		};
+
+        /*
+         * Toggle button not used currently... but there is logic that can be used to toggle.
+         */
+
+        // Show Public Log defaults to false
+        $scope.hidePublic = true;
+
+        // Toggle Public Log
+        $scope.togglePublic = function() {
+            if($scope.hidePublic){
+                $scope.hidePublic = false;
+            } else {
+                $scope.hidePublic = true;
+            }
+        };
+
+	}
+]);
+
+//Change the html background to match due to styling of angular-material on the height style of body.
+d3.select('html').style('background-color', '#222222');
